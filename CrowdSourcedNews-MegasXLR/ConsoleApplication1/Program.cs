3bbae93e56f6d392f91334f76277d7e234799c98
hristo11111 <hristo_brat@yahoo.com>
2013-08-14 16:57:27 +0300

    test console application

---
+++ CrowdSourcedNews-MegasXLR/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using CrowdSourcedNews.Data;
+using CrowdSourcedNews.Data.Migrations;
+using CrowdSourcedNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<CrowdSourcedNewsContext, Configuration>());
+
+            CrowdSourcedNewsContext con = new CrowdSourcedNewsContext();
+
+            User user = new User()
+            {
+                Username = "Name1",
+                Nickname = "nickname1",
+                AuthCode = "123",
+                SessionKey = "456"
+            };
+
+            con.Users.Add(user);
+            con.SaveChanges();
+
+        }
+    }
+}
--- CrowdSourcedNews-MegasXLR/CrowdSourcedNews.Data/Migrations/Configuration.cs
+++ CrowdSourcedNews-MegasXLR/CrowdSourcedNews.Data/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace CrowdSourcedNews.Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CrowdSourcedNews.Data.CrowdSourcedNewsContext>
+    public sealed class Configuration : DbMigrationsConfiguration<CrowdSourcedNews.Data.CrowdSourcedNewsContext>
     {
         public Configuration()
         {
