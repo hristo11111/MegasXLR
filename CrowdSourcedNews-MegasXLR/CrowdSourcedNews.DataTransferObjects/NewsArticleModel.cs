ab5dbd0c70099a9bee6d555da013aa00e7ffbdb8
Vladislav Karamfilov <Vladislav.Karamfilov@gmail.com>
2013-08-14 15:33:55 +0300

    DTOs implemented

---
+++ CrowdSourcedNews-MegasXLR/CrowdSourcedNews.DataTransferObjects/NewsArticleModel.cs
@@ -0,0 +1,53 @@
+﻿namespace CrowdSourcedNews.DataTransferObjects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class NewsArticleModel
+    {
+        private ICollection<byte[]> images;
+        private ICollection<CommentDetails> comments;
+
+        public NewsArticleModel()
+        {
+            this.images = new HashSet<byte[]>();
+            this.comments = new HashSet<CommentDetails>();
+        }
+
+        [DataMember(Name = "id")]
+        public int ID { get; set; }
+
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "content")]
+        public string Content { get; set; }
+
+        [DataMember(Name = "author")]
+        public UserModel Author { get; set; }
+
+        [DataMember(Name = "date")]
+        public DateTime Date { get; set; }
+
+        [DataMember(Name = "rating")]
+        public int Rating { get; set; }
+
+        [DataMember(Name = "images")]
+        public ICollection<byte[]> Images
+        {
+            get { return this.images; }
+
+            set { this.images = value; }
+        }
+
+        [DataMember(Name = "comments")]
+        public ICollection<CommentDetails> Comments
+        {
+            get { return this.comments; }
+
+            set { this.comments = value; }
+        }
+    }
+}
---
+++ CrowdSourcedNews-MegasXLR/CrowdSourcedNews.DataTransferObjects/UserModel.cs
@@ -0,0 +1,41 @@
+﻿namespace CrowdSourcedNews.DataTransferObjects
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class UserModel
+    {
+        private ICollection<NewsArticleModel> newsArticles;
+
+        private ICollection<CommentDetails> comments;
+
+        public UserModel()
+        {
+            this.newsArticles = new HashSet<NewsArticleModel>();
+            this.comments = new HashSet<CommentDetails>();
+        }
+
+        [DataMember(Name = "id")]
+        public int ID { get; set; }
+
+        [DataMember(Name = "nickname")]
+        public string Nickname { get; set; }
+
+        [DataMember(Name = "newsArticles")]
+        public ICollection<NewsArticleModel> NewsArticles
+        {
+            get { return this.newsArticles; }
+
+            set { this.newsArticles = value; }
+        }
+
+        [DataMember(Name = "comments")]
+        public ICollection<CommentDetails> Comments
+        {
+            get { return this.comments; }
+
+            set { this.comments = value; }
+        }
+    }
+}
